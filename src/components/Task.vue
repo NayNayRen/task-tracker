0f079aec8d334eaff0cb0b8395cede70cfe3af7e
Nate <jeditendencies35@gmail.com>
2023-06-28 15:44:13 -0400

    added fa cdn, styled solo task, added ternary for reminded class

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,5 +1,11 @@
 <template>
-  <h3>{{ task.text }}</h3>
+  <!-- v-bind the class and you can do a ternary -->
+  <!-- if task.reminder is true add reminder class, else add nothing -->
+  <!-- still keep the default task class -->
+  <div :class="[task.reminder ? 'reminder' : '', 'task']">
+    <h3>{{ task.text }} <i class="fas fa-times"></i></h3>
+    <p>{{ task.day }}</p>
+  </div>
 </template>
 
 <script>
@@ -13,8 +19,37 @@ export default {
 };
 </script>
 
-<style scoped></style>
+<style>
+.fas {
+  color: red;
+}
+
+.task {
+  background: #f4f4f4;
+  cursor: pointer;
+  margin: 5px;
+  padding: 10px 20px;
+}
+
+.task h3 {
+  -ms-flex-align: center;
+  -ms-flex-pack: justify;
+  -webkit-box-align: center;
+  -webkit-box-pack: justify;
+  align-items: center;
+  display: -ms-flexbox;
+  display: -webkit-box;
+  display: flex;
+  justify-content: space-between;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+</style>
 <!-- good practice to break down the UI into indivdual pieces -->
+<!-- added font awesome cdn to ./public/index.html -->
+<!-- added font awesome icon to solo task h3 -->
 <!-- takes a single task as a prop and the type is an Object -->
 <!-- import into Tasks.vue and register it as a component -->
 <!-- you now have access to each solo task -->
